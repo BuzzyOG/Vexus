e9cecf1ae44617f973ffd43e6a3b73c7574e8814
Robert Calvert <rcalvert@live.com>
2014-12-29 12:44:17 +1300

    Added target notification in tphere command Finished tphere command

--- VexusCore/src/main/java/pw/vexus/core/commands/TpHereCommand.java
+++ VexusCore/src/main/java/pw/vexus/core/commands/TpHereCommand.java
@@ -9,7 +9,7 @@ import pw.vexus.core.VexusCore;
 
 @CommandMeta(description = "Teleport the target player to you.")
 @CommandPermission("vexus.tphere")
-public class TpHereCommand extends VexusCommand {
+public final class TpHereCommand extends VexusCommand {
 
     public TpHereCommand() {
         super("tphere");
@@ -28,5 +28,6 @@ public class TpHereCommand extends VexusCommand {
         tPlayer.teleport(sPlayer.getLocation());
 
         player.sendMessage(VexusCore.getInstance().getFormat("player-tped-here", new String[]{"<player>", target.getName()}));
+        target.sendMessage(VexusCore.getInstance().getFormat("target-tped-here", new String[]{"<player>", player.getName()}));
     }
 }
